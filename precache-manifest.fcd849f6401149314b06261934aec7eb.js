a9d987417116b8235ac3f3983eead24148ad3c96
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-08-18 06:18:47 +0000

    Deploy COSCUP/CCIP-HDP to github.com/COSCUP/CCIP-HDP.git:gh-pages

---
+++ precache-manifest.fcd849f6401149314b06261934aec7eb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "707df37318cc0325d2c1",
+    "url": "/css/app.7eaf1274.css"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/css/chunk-vendors.5e44303a.css"
+  },
+  {
+    "revision": "ae9a6a9deb83728f73036f522aafd290",
+    "url": "/index.html"
+  },
+  {
+    "revision": "707df37318cc0325d2c1",
+    "url": "/js/app.099215db.js"
+  },
+  {
+    "revision": "c27013b4fd50412baf2f",
+    "url": "/js/chunk-vendors.db9426a7.js"
+  },
+  {
+    "revision": "cb9728c6ed359443518f",
+    "url": "/js/declaration.968ce791.js"
+  },
+  {
+    "revision": "f9c18d4979784c4cbdad",
+    "url": "/js/fill.b5a6e899.js"
+  },
+  {
+    "revision": "c922184fbf56514e53ca",
+    "url": "/js/status.4f8e1230.js"
+  },
+  {
+    "revision": "28bf2ab99af2d0770d51a57d31b2f113",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
